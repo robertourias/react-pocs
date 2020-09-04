3f1d5029ccaff847d21b7cc596c9546dba7367c8
Roberto Nicoletti <roberto.urias@gmail.com>
2020-09-04 20:22:09 -0300

    HuntWeb: Criação de estado

--- huntweb/src/pages/main/index.js
+++ huntweb/src/pages/main/index.js
@@ -2,19 +2,26 @@ import React, { Component} from 'react';
 import api from "../../services/api";
 
 export default class Main extends Component {
+    state = {
+        products: []
+    };
+
     componentDidMount() {
         this.loadProducts();
     }
 
     loadProducts = async () => {
         const response = await api.get('/products');
-
-        
+        this.setState({ products: response.data.docs });
     };
 
     render() {
         return (
-            <h1>Hello Rocketseat</h1>
+            <div className="product-list">
+                {this.state.products.map(product => (
+                    <h2 key={product._id}>{product.title}</h2>
+                ))}
+            </div>
         );
     }
 }
