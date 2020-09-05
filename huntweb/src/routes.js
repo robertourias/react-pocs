53a21f51e994341017e8574f67661ad2f9f79521
Roberto Nicoletti <roberto.urias@gmail.com>
2020-09-05 20:37:02 -0300

    Huntweb: Criando roteamento

---
+++ huntweb/src/routes.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Main from './pages/main/index';
+import Product from './pages/product';
+
+const Routes = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={Main} />
+            <Route path="/products/:id" component={Product} />
+        </Switch>
+    </BrowserRouter>
+);
+
+export default Routes;
