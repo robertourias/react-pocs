7218c21ebaa7168c15fb5951c4def16b2c5cb8cc
Roberto Nicoletti <roberto.urias@gmail.com>
2020-09-16 19:12:05 -0300

    Nextjs: About page

---
+++ nextjs-poc/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas porro maiores doloribus dicta et exercitationem illo sequi consectetur velit, eius cumque veniam sapiente consequuntur explicabo facilis, suscipit perspiciatis ipsa dolorem?</p>
+    </div>
+  );
+};
+
+export default About;
