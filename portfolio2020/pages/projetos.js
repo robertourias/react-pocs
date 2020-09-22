1fdcfe588a76f50e998d85f2f62282d7c41e2d53
Roberto Nicoletti <roberto.urias@gmail.com>
2020-09-22 20:03:07 -0300

    Portfolio 2020: Criando página de projetos

--- portfolio2020/pages/projetos.js
+++ portfolio2020/pages/projetos.js
@@ -1,3 +1,14 @@
+import Link from 'next/link'
+
 export default function Projetos() {
-    return <h1>Projetos</h1>
-  }
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  )
+}
