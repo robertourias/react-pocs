11f1393c6953ada469441a4693befc973cbd27c6
Roberto Nicoletti <roberto.urias@gmail.com>
2020-09-22 20:35:53 -0300

    Portfolio 2020: Configurando Sass

--- portfolio2020/next.config.js
+++ portfolio2020/next.config.js
@@ -1,7 +1,5 @@
-const path = require('path')
-
-module.exports = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'shared/stylesheets')],
-  },
-}
+// next.config.js
+const withSass = require('@zeit/next-sass')
+module.exports = withSass({
+  /* config options here */
+})
--- portfolio2020/pages/projetos.js
+++ portfolio2020/pages/projetos.js
@@ -1,14 +1,15 @@
 import Link from 'next/link'
+import "../styles.scss";
 
 export default function Projetos() {
   return (
-    <>
+    <div className="example globalClass">
       <h1>First Post</h1>
       <h2>
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </div>
   )
 }
