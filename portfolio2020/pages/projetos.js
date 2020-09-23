2a807d6c0dff5006043126d6ad0be9d9c128cd3e
Roberto Nicoletti <roberto.urias@gmail.com>
2020-09-22 21:06:55 -0300

    Portfolio 2020: Criando Head na página do projeto

--- portfolio2020/pages/projetos.js
+++ portfolio2020/pages/projetos.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Link from 'next/link'
 import "../styles.scss";
 import "../shared/stylesheets/pages/projects.scss";
@@ -5,6 +6,9 @@ import "../shared/stylesheets/pages/projects.scss";
 export default function Projetos() {
   return (
     <div className="example globalClass">
+        <Head>
+            <title>TEste</title>
+        </Head>
       <h1 className="projects">First Post</h1>
       <h2>
         <Link href="/">
