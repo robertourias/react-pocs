a6922558076401a7e4d45a7e2c6246cf90bce2f3
Roberto Nicoletti <roberto.urias@gmail.com>
2020-09-23 21:11:19 -0300

    Portfolio 2020: Criando componente Layout

---
+++ portfolio2020/components/layout.js
@@ -0,0 +1,7 @@
+import '../styles.scss';
+
+function Layout({ children }) {
+    return <div className="container">{children}</div>
+  }
+  
+  export default Layout
--- portfolio2020/pages/projetos.js
+++ portfolio2020/pages/projetos.js
@@ -1,20 +1,23 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Layout from '../components/layout';
 import "../styles.scss";
 import "../shared/stylesheets/pages/projects.scss";
 
 export default function Projetos() {
   return (
-    <div className="example globalClass">
-        <Head>
-            <title>TEste</title>
-        </Head>
-      <h1 className="projects">First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
-    </div>
+      <Layout>
+        <div className="example globalClass">
+            <Head>
+                <title>TEste</title>
+            </Head>
+        <h1 className="projects">First Post</h1>
+        <h2>
+            <Link href="/">
+            <a>Back to home</a>
+            </Link>
+        </h2>
+        </div>
+    </Layout>
   )
 }
