856d9396a46a168558f1fc61b649e4a3d105e54f
Roberto Nicoletti <roberto.urias@gmail.com>
2020-09-25 23:27:11 -0300

    Portfolio 2020: Criando página carregando posts

--- portfolio2020/pages/index.js
+++ portfolio2020/pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -14,6 +15,29 @@ export default function Home() {
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
+      <section className="headingMd padding1px">
+        <h2 className="headingLg">Blog</h2>
+        <ul className="list">
+          {allPostsData.map(({ id, date, title }) => (
+            <li className="listItem" key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
